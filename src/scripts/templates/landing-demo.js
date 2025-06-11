8ebc575385d9fb2c3cfce825383692d8a64b2837
ahmadfatih16 <152517428+ahmadfatih16@users.noreply.github.com>
2025-06-11 14:01:53 +0700

    membenahi landingpage demo

--- src/scripts/templates/landing-demo.js
+++ src/scripts/templates/landing-demo.js
@@ -1,10 +1,11 @@
 export default function LandingDemo() {
   return `
-    <section id="demo" class="container mx-auto py-16 md:py-24 bg-neutral-900 text-center">
-      <h1 class="text-white text-5xl font-bold py-15">
+    <section id="demo" class="container mx-auto py-16 md:py-24 bg-neutral-900 text-center
+      min-h-screen flex flex-col justify-center items-center">
+      <h1 class="text-white text-5xl font-bold mb-8 pb-10">
         <span class="text-emerald-400">Demo</span>nstrasi
       </h1>
-      <video controls class="mx-auto w-full max-w-5xl mt-6 rounded-lg shadow-lg">
+      <video controls class="mx-auto w-full max-w-5xl rounded-lg shadow-lg mt-10">
         <source src="/public/video/contoh.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
